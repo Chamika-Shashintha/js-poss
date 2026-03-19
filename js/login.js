5c014a0b78d7d1ce4abca6b9b5c3460a924312be
Chamika Shashintha <chamikashashintha1008gmail.com>
2026-03-19 20:58:39 +0530

    firebase

--- js/login.js
+++ js/login.js
@@ -1,5 +1,18 @@
 const login=()=>{
-   
+console.log('running!...');
+     const email = $('#email').val()
+     const password = $('#password').val()  
+
+   firebase
+   .auth()
+   .signInWithEmailAndPassword(email,password)
+   .then((resp)=>{
+     console.log(resp);
+     console.log(resp.user);
+   })
+   .catch((error)=>{
+     console.log(error);
+   });
 }
 
 const createAnAccount=()=>{
--- js/register.js
+++ js/register.js
@@ -1,6 +1,21 @@
 const register=()=>{
 
-}
+    const email = $('#email').val()
+    const password = $('#password').val()
+
+
+        firebase
+        .auth()
+        .createUserWithEmailAndPassword(email,password)
+        .then((cred)=>{
+            console.log(cred);
+            console.log(cred.user);
+        })
+        .catch((error)=>{
+            console.log(error);
+        })
+    }
+
 
 const AllredyHaveAnAccount=()=>{
     window.location.replace('login.html')

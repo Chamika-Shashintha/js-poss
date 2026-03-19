e64fe45ff033d8447b47f681c525ed10ed5b8293
Chamika Shashintha <chamikashashintha1008gmail.com>
2026-03-19 20:57:52 +0530

    firebase

---
+++ js/firebaseCnfig.js
@@ -0,0 +1,12 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBeZ2aTVUB9MFA1D2OOSTmPMyf5kuT0DQo",
+  authDomain: "pos-system-d331e.firebaseapp.com",
+  projectId: "pos-system-d331e",
+  storageBucket: "pos-system-d331e.firebasestorage.app",
+  messagingSenderId: "308155559867",
+  appId: "1:308155559867:web:e7994517e9eadddf8cde9e",
+  measurementId: "G-FB3GVT01D4"
+};
+
+firebase.initializeApp(firebaseConfig);

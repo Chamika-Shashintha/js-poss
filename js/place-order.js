47663e35d82a7e8344abf968100249dbca58a21a
Chamika Shashintha <chamikashashintha1008gmail.com>
2026-03-21 00:48:58 +0530

    place order

--- js/place-order.js
+++ js/place-order.js
@@ -1,1 +1,14 @@
-loadCustomerIds()
+const loadCustomerIds=()=>{
+    $('#customer-id').emty;
+}
+
+const firestore = firebase.firestore();
+firestore
+    .collection('customers')
+    .get()
+    .then((recodes =>{
+        recodes.forEach((result)=>{
+            const option = $('<option></optiion>').val(result.id).text(result.id)
+            $('#customer-id').append(option)
+        })
+    }))

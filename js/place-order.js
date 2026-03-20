35f8c23de736b6595ba5ee4cf3d9701ce233bf52
S.W.B Chamika Shashintha Bandara <chamikashashintha1008@gmail.com>
2026-03-20 19:44:37 +0000

    placeOrder

--- js/place-order.js
+++ js/place-order.js
@@ -12,3 +12,34 @@ firestore
             $('#customer-id').append(option)
         })
     }))
+    
+$('#customer-id').on("change",function (){
+    const customerId=$(this).val();
+    const firestore = firebase.firestore();
+    firestore
+        .collection('customers')
+        .doc(customerId)
+        .get().then((response)=>{
+        if (response.exists) {
+            const data = response.data();
+            $('#name').val(data.name);
+            $('#address').val(data.address);
+            $('#salary').val(data.salary)
+        }
+    })
+});
+$('#item-id').on("change",function (){
+    const itemId=$(this).val();
+    const firestore = firebase.firestore();
+    firestore
+        .collection('items')
+        .doc(itemId)
+        .get().then((response)=>{
+        if (response.exists) {
+            const data = response.data();
+            $('#description').val(data.description);
+            $('#unit-price').val(data.unitPrice);
+            $('#qty-on-hand').val(data.qtyOnHand)
+        }
+    })
+});
